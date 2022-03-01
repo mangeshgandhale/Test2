31ecc923003c03da5b41670f0f8f8b6a1e6a98ba
mangesh <mangesh.gandhale@gmail.com>
2022-02-28 22:50:36 -0500

    changesh design

---
+++ MVC3Layer/Controllers/teController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC3Layer.Controllers
+{
+    public class teController : Controller
+    {
+        // GET: te
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: te/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: te/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: te/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: te/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: te/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: te/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: te/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
